7c52cf70f84acf9df5eef7ff8f816e3200556369
Roomi <javed.roomi@gmail.com>
2026-07-20 23:16:51 +0100

    new file added

--- tests/Dialogbox.spec.ts
+++ tests/Dialogbox.spec.ts
@@ -19,3 +19,29 @@ test('dialogbox', async({page}) => {
     //await expect(page.locator('tbody tr').nth(1).locator('td').nth(5)).not.toHaveText('fat@yandex.ru')
 
 })
+
+test('webtable1', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+    const targetrow =  page.getByRole("row", {name: 'twitter@outlook.com'})
+    await targetrow.locator('.nb-edit').click()
+    await page.locator('input-editor').getByPlaceholder('Age').clear()
+    await page.locator('input-editor').getByPlaceholder('Age').fill('18')
+    await page.locator('.nb-checkmark').click()
+})
+
+test ('webtablebyId', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+    await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+    const targetrowbyId = page.getByRole('row', {name: '11'}).filter({has: page.locator('td').nth(1).getByText('11')})
+    await targetrowbyId.locator('.nb-edit').click()
+    await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+    await page.locator('.nb-checkmark').click()
+    expect(targetrowbyId.locator('td').nth(5)).toHaveText('test@test.com')
+
+
+
+
+})
